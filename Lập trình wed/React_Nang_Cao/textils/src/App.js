cfee3d8c70e8a1810c7beb25153771d99b62e278
HIEU NGUYEN THANH <thanhhieu961906@gmail.com>
2023-06-07 08:50:11 +0700

    0607

--- Lập trình wed/React_Nang_Cao/textils/src/App.js
+++ Lập trình wed/React_Nang_Cao/textils/src/App.js
@@ -1,25 +1,57 @@
-
 import './App.css';
-let name =  "<b>Thanh Hieu</b>"
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import About from './Components/About';
+import React, { useState } from 'react';
+import Alert from './Components/Alter';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
     <>
-    <div className="useName">
-          Hello {name} {/* tất cả những thứ bỏ trong ngoặc nhọn đều là javascript */}
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}  />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Routes>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/about" element={<About mode={mode} />}/>
+          <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}/>
+    </Routes>
     </div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Shop</li>
-          <li>Blog</li>
-        </ul>
-      </nav>
-      <div className="container-content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Voluptatibus fuga hic eligendi eius aspernatur incidunt ipsam tempora tenetur explicabo reiciendis, minus id praesentium facilis qui atque obcaecati, eveniet delectus esse.
-      </div>
-    </>
+    </Router>
+    </> 
   );
 }
 
